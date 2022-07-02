9eacacd330fa77c39b8b9779400c3fba37186a71
VadimRogo <106582314+VadimRogo@users.noreply.github.com>
2022-07-02 20:11:06 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,46 +1,101 @@
-from calendar import monthcalendar
+from itertools import count
 import json
+from ast import Num
+import json, time
+from statistics import quantiles
+from typing import Counter
 import pip._vendor.requests  as requests
-import re, time
+import numpy as np
 import matplotlib.pyplot as plt
-# defining key/request url
 key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-  
-# requesting data from url
-data = requests.get(key)  
-data = data.json()
 prices = []
-prices.append(float(data['price']))
-counter = [0]
+times = int(input())
+money = 1000
+Counter = 0
+JsonTikets= []
+moneys = []
+Countered = []
+class Tiket:
+    count = 0
+    def __init__(self, num, quantity, price, time, sold):
+        self.Counter = Counter
+        self.quantity = quantity
+        self.price = price
+        self.time = time
+        self.sold = sold
+        # print('We but {} by {} at {}'.format(self.quantity, self.price, self.time))
 
-print(float(data['price']))
+    def MakeTiket(self):
+        NewTiket = json.dumps({'Num': self.num, 'Quantity': self.quantity, 'Price': price, 'Time': time, 'sold': sold})
 
-#Простая функция для получения графика через библиотеку Matplotlib
-for i in range (1, 30):
+
+
+def BuyingProcess(price):
+    global money
+    TwentyPrecent = money / 100 * 20
+    money -= TwentyPrecent
+    quantity = round((TwentyPrecent) / float(price), 3)
+    WTime = time.strftime('%X %x')
+    sold = False
+    print("We BUY in {}".format(price))
+    T1 = Tiket(Counter, quantity, price, WTime, sold)
+    JsonTiket = {"Counter": T1.Counter, "Price": T1.price,\
+          "Time": T1.time, "quantity": T1.quantity, "SOLD": T1.sold, }
+    JsonTikets.append(JsonTiket)
+
+def SellingProcess(price):
+    global money
+    for Tiket in JsonTikets:
+        if Tiket['SOLD'] == False:
+            print('We SOLD some BTC')
+            money += Tiket['quantity'] * price
+            Tiket['SOLD'] = True
+        else:
+            print('We don"t have something for SELL')
+
+def CentreWallue(prices):
+    if max(prices) - min(prices) > max(prices) * 0.05:
+        prices = np.array(prices)
+        prices += max(prices) * 0.05
+        print("Input was centered")
+
+def MakeGraphBTC(prices, counter):
+    x = prices
+    y = counter
+
+    plt.plot(y, x)
+    plt.xlabel('Price')
+    plt.ylabel('Counter')
+    plt.show()
+    
+def MakeGraphMoneyBag(moneys, counter):
+    x = moneys
+    y = counter
+
+    plt.plot(y, x)
+    plt.xlabel('Money')
+    plt.ylabel('Counter')
+    plt.show()
+
+for i in range(times):
+    Countered.append(i)
+    moneys.append(money)
     data = requests.get(key)
     data = data.json()
-    price = (float(data['price']))
-    prices.append(float(price))    
-    counter.append(float(i))
-    counter.append(i)
+    price = round(float(data['price']), 3)
+    prices.append(price)
+    if price <= max(prices):
+        Counter += 1
+        BuyingProcess(price)
+    elif price >= min(prices):
+        SellingProcess(price)
     print(price)
+    # CentreWallue(prices)
+    print(money)
     time.sleep(1)
+print(len(moneys), len(Countered), len(prices))
 
-#Имитация кошелька
-MoneyBag = 100 * prices[len(prices)-1] / prices[0]
-print(MoneyBag)
-
-
-#Вызов графика в графическом интерфесе
-plt.plot(counter, prices, label = 'line 1')
-plt.xlabel('Price')
-plt.ylabel('Times')
-plt.title('BTSUSD')
-plt.show()
-
-
-
-
-# print(data['price'])
-
-# print(f"{data['symbol']} price is {data['price']}")
+MakeGraphBTC(prices, Countered)
+MakeGraphMoneyBag(moneys, Countered)
+print(json.dumps(JsonTikets, sort_keys=False, indent=4))
+print(Counter)
